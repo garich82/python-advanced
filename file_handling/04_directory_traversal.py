db3a41c0cbc09e496d9afd20d8ca7230f680e13d
garich82 <garo@gbg.bg>
2023-06-03 10:03:44 +0100

    Added changes from mobile laptop

--- file_handling/04_directory_traversal.py
+++ file_handling/04_directory_traversal.py
@@ -1,15 +1,15 @@
 import os
 
-# Variant 1 - Manual entry of the directory
-# directory = "c:\\Users\\....."  # Replace with your chosen directory path
+directory = input("Enter the directory path: ")
 
-# Variant 2 - Directory is by default the directory of the current .py file
-directory = os.path.dirname(os.path.abspath(__file__))
+if not os.path.exists(directory):
+    print("Error: The specified directory does not exist.")
+    exit()
 
 files = {}
 
 try:
-    for entry in os.scandir(directory):
+    for entry in os.scandir(directory):  # Getting the files in the main directory
         if entry.is_file():
             file_name = entry.name
             extension = os.path.splitext(file_name)[1]
@@ -19,21 +19,31 @@ try:
 
             files[extension].append(file_name)
 
+    subdirectories = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
+
+    for subdir in subdirectories:  # Getting the files in each of the immediate subdirectories
+        subdir_path = os.path.join(directory, subdir)
+        for entry in os.scandir(subdir_path):
+            if entry.is_file():
+                file_name = entry.name
+                extension = os.path.splitext(file_name)[1]
+
+                if extension not in files:
+                    files[extension] = []
+
+                files[extension].append(file_name)
+
     sorted_extensions = sorted(files.keys())
 
-    with open(os.path.join(directory, 'report.txt'), 'w') as report_file:
-
+    with open(os.path.join(directory, 'report.txt'), 'w') as report_file:  # Generating the report file
         for extension in sorted_extensions:
             report_file.write(f"{extension}\n")
             files[extension].sort()
             for file_name in files[extension]:
-                report_file.write(f"- - - {file_name}\n")
+                report_file.write(f"- - - {os.path.basename(file_name)}\n")
             report_file.write("\n")
 
     print("Report generated successfully.")
 
 except FileNotFoundError:
     print("Error: The specified directory does not exist.")
-
-
-
--- test.py
+++ test.py
@@ -1,54 +1,107 @@
 import random
 
 
-def calculate_damage():
-    hits = sum(random.choices([0, 1], weights=[1/3, 2/3]) for _ in range(18))
-    return hits
+class Ship:
+    def __init__(self, ship_type, fleet_number):
+        if ship_type == 'Carrack':
+            self.ship_name = f'Carrack {fleet_number}'
+            self.attack = 18
+        elif ship_type == 'Cog':
+            self.ship_name = f'Cog {fleet_number}'
+            self.attack = 11
+        else:
+            raise ValueError("Invalid ship type")
+
+        self.hit_points = 40
+
+    def __str__(self):
+        return f"{self.ship_name} - {self.hit_points} HP"
 
 
-def calculate_fight():
-    fleet_a_size = int(input("Enter the size of Fleet A: "))
-    fleet_b_size = int(input("Enter the size of Fleet B: "))
+def calculate_damage(num_shots):
+    total_damage = 0
+    for _ in range(num_shots):
+        if random.choices([0, 1], weights=[1/3, 2/3])[0] == 1:
+            total_damage += 1
+    return total_damage
 
-    fleet_a = [40 for _ in range(fleet_a_size)]
-    fleet_b = [40 for _ in range(fleet_b_size)]
+
+def generate_fleet():
+    fleet_a_carracks = int(input("Enter the number of Carracks in Fleet A: "))
+    fleet_a_cogs = int(input("Enter the number of Cogs in Fleet A: "))
+    fleet_b_carracks = int(input("Enter the number of Carracks in Fleet B: "))
+    fleet_b_cogs = int(input("Enter the number of Cogs in Fleet B: "))
+
+    fleet_a = [Ship('Carrack', i) for i in range(1, fleet_a_carracks + 1)] + [Ship('Cog', i) for i in range(1, fleet_a_cogs + 1)]
+    fleet_b = [Ship('Carrack', i) for i in range(1, fleet_b_carracks + 1)] + [Ship('Cog', i) for i in range(1, fleet_b_cogs + 1)]
+
+    return fleet_a, fleet_b
+
+
+def print_fleet(fleet):
+    for ship in fleet:
+        print(ship)
+
+
+def assign_damage(damage, fleet):
+    num_ships = len(fleet)
+    for _ in range(damage):
+        if num_ships > 0:
+            ship_index = random.randint(0, num_ships - 1)
+            fleet[ship_index].hit_points -= 1
+            if fleet[ship_index].hit_points <= 0:
+                fleet.pop(ship_index)
+                num_ships -= 1
+
+
+def battle_round(fleet_a, fleet_b, round_num, fleet_a_initial_count, fleet_b_initial_count):
+    print(f"--- Round {round_num} ---")
+
+    # Calculate damage for Fleet A
+    damage_a = sum([calculate_damage(ship.attack) for ship in fleet_a])
+
+    # Calculate damage for Fleet B
+    damage_b = sum([calculate_damage(ship.attack) for ship in fleet_b])
+
+    # Assign damage to ships in Fleet B
+    assign_damage(damage_a, fleet_b)
+
+    # Assign damage to ships in Fleet A
+    assign_damage(damage_b, fleet_a)
+
+    # Calculate losses for each fleet
+    fleet_a_losses = fleet_a_initial_count - len(fleet_a)
+    fleet_b_losses = fleet_b_initial_count - len(fleet_b)
+
+    # Print round summary
+    print(f"Fleet A inflicted {damage_a} damage and lost {fleet_a_losses} ships.")
+    print(f"Fleet B inflicted {damage_b} damage and lost {fleet_b_losses} ships.")
+
+    # Print state of fleets
+    print("\nFleet A:")
+    print_fleet(fleet_a)
+
+    print("\nFleet B:")
+    print_fleet(fleet_b)
+
+    print()
+
+
+def simulate_battle():
+    fleet_a, fleet_b = generate_fleet()
+    fleet_a_initial_count = len(fleet_a)
+    fleet_b_initial_count = len(fleet_b)
+    round_num = 1
 
     while fleet_a and fleet_b:
-        # Fleet A attacks Fleet B
-        for _ in range(fleet_a_size):
-            if fleet_b:
-                damage = calculate_damage()
-                ship_index = random.randint(0, len(fleet_b) - 1)
-                fleet_b[ship_index] -= damage
-                if fleet_b[ship_index] <= 0:
-                    fleet_b.pop(ship_index)
+        battle_round(fleet_a, fleet_b, round_num, fleet_a_initial_count, fleet_b_initial_count)
+        round_num += 1
 
-        # Fleet B attacks Fleet A
-        for _ in range(fleet_b_size):
-            if fleet_a:
-                damage = calculate_damage()
-                ship_index = random.randint(0, len(fleet_a) - 1)
-                fleet_a[ship_index] -= damage
-                if fleet_a[ship_index] <= 0:
-                    fleet_a.pop(ship_index)
-
-    if fleet_a:
-        winner = "Fleet A"
-        winning_fleet = fleet_a
-        losing_fleet = fleet_b
-    else:
-        winner = "Fleet B"
-        winning_fleet = fleet_b
-        losing_fleet = fleet_a
-
-    print("Winner:", winner)
-    print("Number of ships sunk by the winning fleet:", len(losing_fleet))
-    print("Number of ships lost:", len(winning_fleet))
-    print("Ships that survived:")
-
-    for i, hit_points in enumerate(winning_fleet, start=1):
-        print(f"Ship {i} - {hit_points}/40")
+    print("--- Battle Summary ---")
+    print(f"Total Rounds: {round_num - 1}")
+    print(f"Fleet A ships remaining: {len(fleet_a)}")
+    print(f"Fleet B ships remaining: {len(fleet_b)}")
 
 
-# Example usage:
-calculate_fight()
+# Start the battle simulation
+simulate_battle()
